cdcceefeadadeaa3c54bd7e06dd11dfd99ca36b6
Miguel Artacho <tatxo@zhora.local>
2025-12-26 08:35:41 +0100

    feat: Add EFS mount points to containers

--- 1/alb.tf
+++ 1/alb.tf
@@ -38,3 +38,35 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.wordpress-tg.arn
   }
 }
+
+# Security groups
+resource "aws_security_group" "alb-sg" {
+  name        = "alb-sg"
+  description = "Security Group for the ALB"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
--- 1/ecs.tf
+++ 1/ecs.tf
@@ -3,6 +3,7 @@ resource "aws_ecs_cluster" "wordpress" {
   name = var.ecs_cluster_name
 }
 
+# MySQL service
 resource "aws_ecs_service" "mysql_service" {
   name                = "mysql-service"
   cluster             = aws_ecs_cluster.wordpress.id
@@ -19,6 +20,7 @@ resource "aws_ecs_service" "mysql_service" {
   }
 }
 
+#WordPress service
 resource "aws_ecs_service" "wordpress" {
   name                 = "wordpress-service"
   cluster              = aws_ecs_cluster.wordpress.id
@@ -48,6 +50,14 @@ resource "aws_ecs_task_definition" "mysql" {
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs-task-role.arn
+
+  volume {
+    name = "mysql-storage"
+    efs_volume_configuration {
+      file_system_id = aws_efs_file_system.mysql-efs.id
+      transit_encryption = "ENABLED"
+    }
+  }
 
   container_definitions = jsonencode([
     {
@@ -80,6 +90,7 @@ resource "aws_ecs_task_definition" "mysql" {
 }
 
 resource "aws_ecs_task_definition" "wordpress" {
+  :w
   family                   = "wordpress"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
@@ -87,7 +98,17 @@ resource "aws_ecs_task_definition" "wordpress" {
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs-task-role.arn
   #    task_role_arn            = aws_iam_role.ecs-task-role.arn
-
+  volume {
+    name = "wp-storage"
+    efs_volume_configuration {
+      file_system_id        = aws_efs_file_systen.wp-efs.id
+      transit_encryption    = "ENABLED"
+      authorization_config {
+        access_point_id = aws_efs_access_point.wp-ap.id
+        iam             = "ENABLED"
+      }
+    }
+  }
   container_definitions = jsonencode([
     {
       name     = "wordpress"
@@ -116,4 +137,87 @@ resource "aws_ecs_task_definition" "wordpress" {
       }
     }
   ])
+}
+
+
+resource "aws_security_group" "wordpress-sg" {
+  vpc_id = aws_vpc.default.id
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    #    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.alb-sg.id]
+  }
+
+  ingress {
+    from_port   = 1024
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "wordpress-sg"
+  }
+}
+
+resource "aws_security_group" "mysql-sg" {
+  name        = "mysql-sg"
+  description = "Access to the RDS instances from the VPC"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.wordpress-sg.id]
+  }
+
+  ingress {
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  ingress {
+    from_port   = 1024
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mysql-sg"
+  }
 }
